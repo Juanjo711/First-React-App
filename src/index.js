0ce8699e1be6effc21becc20c159a42b3b59512c
Juan Estrada <juankomc711@gmail.com>
2023-06-19 13:19:09 -0500

    mostrar objetos y booleanos como string, quitar etiquetas div

--- src/index.js
+++ src/index.js
@@ -6,19 +6,28 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 function Saludo() {
   const nombre = "Juan";
   const estatura = 178;
+  const activo = true;
+
+  const usuario = {
+    nombreUsuario: "Juan.e",
+    correo: "juan@mail.com",
+  };
 
   return (
-    <div>
+    <>
       <h1>Hola, {nombre} 👋</h1>
       <p>{estatura > 160 ? "Eres alto" : "Eres bajo"}</p>
+      <p>{JSON.stringify(usuario)}</p>
+      <p>{usuario.correo}</p>
+      <p>{activo.toString()}</p>
       <h2>Componente de prueba</h2>
       <p>Texto de prueba</p>
-    </div>
+    </>
   );
 }
 
 root.render(
-  <div>
+  <>
     <Saludo />
-  </div>
+  </>
 );
