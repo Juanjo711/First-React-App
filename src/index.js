f33831137c099ce14f21a2f58d0c26a390b1e1cb
Juan Estrada <juankomc711@gmail.com>
2023-06-19 12:55:51 -0500

    Primer componente de React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+function Saludo() {
+    const nombre = "Juan"
+
+    return <div>
+                <h1>Hola, {nombre}</h1>
+                <h2>Componente de prueba</h2>
+                <p>Texto de prueba</p>
+            </div>
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div>
+        <Saludo />
+    </div>
+)
