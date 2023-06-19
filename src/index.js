d80e165e04134eb2e20c28c4cf418e2e7b6aedae
Juan Estrada <juankomc711@gmail.com>
2023-06-19 13:07:22 -0500

    Operador ternario en un componente

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Saludo() {
-    const nombre = "Juan"
+  const nombre = "Juan";
+  const estatura = 178;
 
-    return <div>
-                <h1>Hola, {nombre}</h1>
-                <h2>Componente de prueba</h2>
-                <p>Texto de prueba</p>
-            </div>
+  return (
+    <div>
+      <h1>Hola, {nombre} 👋</h1>
+      <p>{estatura > 160 ? "Eres alto" : "Eres bajo"}</p>
+      <h2>Componente de prueba</h2>
+      <p>Texto de prueba</p>
+    </div>
+  );
 }
 
 root.render(
-    <div>
-        <Saludo />
-    </div>
-)
+  <div>
+    <Saludo />
+  </div>
+);
